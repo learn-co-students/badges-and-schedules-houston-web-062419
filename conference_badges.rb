d64178e1ebe00b988a96831a1a7905134c3a150d
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 22:57:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker (name)
+  return String.new("Hello, my name is #{name}.")
+end
+def batch_badge_creator (array)
+  badge_array = []
+  array.each {|name| badge_array.push(badge_maker(name))}
+  return badge_array
+end
+
+def assign_rooms (array)
+  rooms_array = []
+  n = 0
+  array.each {|name| rooms_array.push("Hello, #{name}! You'll be assigned to room #{n += 1}!")}
+  return rooms_array
+end
+
+def printer (array)
+  batch_badge_creator(array).each {|str| puts str}
+  assign_rooms(array).each {|str| puts str}
+
+  #puts batch_badge_creator(array)
+  #puts assign_rooms(array)
+end
